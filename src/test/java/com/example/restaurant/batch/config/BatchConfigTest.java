d3c963261a9461b8e9123446fe13563c0e58b7c4
miyeon396 <miyeon396@gmail.com>
2024-08-15 12:14:50 +0900

    잡테스트 데이터 체크 부분 제거

--- src/test/java/com/example/restaurant/batch/config/BatchConfigTest.java
+++ src/test/java/com/example/restaurant/batch/config/BatchConfigTest.java
@@ -1,6 +1,5 @@
 package com.example.restaurant.batch.config;
 
-import com.example.restaurant.repository.RestaurantInfoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
@@ -11,9 +10,7 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,12 +22,6 @@ class BatchConfigTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Autowired
-    private RestaurantInfoRepository restaurantInfoRepository;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @Test
     public void 잡_수행_성공테스트() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
@@ -41,15 +32,6 @@ class BatchConfigTest {
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
         assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
         assertEquals("COMPLETED WITH SKIPS", jobExecution.getExitStatus().getExitCode());
-
-//        List<RestaurantInfo> restaurantInfos = restaurantInfoRepository.findAll();
-//        assertThat(restaurantInfos.size() >= 5);
-
-//        String sql = "select step_name, status, commit_count, read_count, write_count from BATCH_STEP_EXECUTION where job_execution_id = ?";
-//        List<Map<String, Object>> metaTable = jdbcTemplate.queryForList(sql, jobExecution.getJobId());
-//        for (Map<String, Object> step : metaTable) {
-//            log.info("meta table row={}", step);
-//        }
 
     }
 
