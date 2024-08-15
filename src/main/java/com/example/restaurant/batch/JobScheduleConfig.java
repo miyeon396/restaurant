aa9037e42219d85c279d316c6e21b2d5d231926d
miyeon396 <miyeon396@gmail.com>
2024-08-15 17:08:07 +0900

    불필요 import 정리

--- src/main/java/com/example/restaurant/batch/JobScheduleConfig.java
+++ src/main/java/com/example/restaurant/batch/JobScheduleConfig.java
@@ -1,18 +1,13 @@
 package com.example.restaurant.batch;
 
-import com.example.restaurant.batch.config.BatchConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Component
