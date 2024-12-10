ca0e1dee185077e405c46cce45846ac00c337764
miyeon.yeon <47619645+miyeon396@users.noreply.github.com>
2024-12-10 17:54:01 +0900

    Update CreatePseudonimizeStepConfig.java

--- src/main/java/com/example/restaurant/batch/jobs/pseudonimize/step/create/CreatePseudonimizeStepConfig.java
+++ src/main/java/com/example/restaurant/batch/jobs/pseudonimize/step/create/CreatePseudonimizeStepConfig.java
@@ -83,4 +83,20 @@ public class CreatePseudonimizeStepConfig {
 
     //in 1h4m31s26ms
 
+    //전략으로 구현한 리더 클래스의 실제 리더에서 파티셔니이 안된다가 이슈 
+    // @Component
+    // public class ReaderStrategyFactory<E> {
+    //     private final Map<TrasferTable, TransferReaderStrategy<E>> trasferReaderMap = new HashMap<>();
+
+    //     public ReaderStrategyFactory(List<TransferReaderStrategy<E>> stList) {
+    //         for (TransferReaderStrategy<E> st : stList) {
+    //             trasferReaderMap.put(st.getTableType(), st);
+    //         }
+    //     }
+
+    //     public TransferTableReaderStrategy<E> getTransferReaderStrategy(TransferTable type) {
+    //         return Optional.ofNullable(trasferReaderMap.get(type)).orElseThrow(() -> new NoSuchElementException("nono : "+type));
+    //     }
+    // }
+
 }
